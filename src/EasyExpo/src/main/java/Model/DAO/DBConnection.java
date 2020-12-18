0628cefea3d3bc482c2345d6ac75bbf7283eae28
gaetanik99 <iulianogaetano99@gmail.com>
2020-12-18 17:06:10 +0100

    Aggiunta Database

--- src/EasyExpo/src/main/java/Model/DAO/DBConnection.java
+++ src/EasyExpo/src/main/java/Model/DAO/DBConnection.java
@@ -1,4 +1,31 @@
 package Model.DAO;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.TimeZone;
+
 public class DBConnection {
+    private static DataSource datasource;
+
+    public static Connection getConnection() throws SQLException {
+        if (datasource == null) {
+            PoolProperties p = new PoolProperties();
+            p.setUrl("jdbc:mysql://localhost:3306/EasyExpo?serverTimezone=" + TimeZone.getDefault().getID());
+            p.setDriverClassName("com.mysql.cj.jdbc.Driver");
+            p.setUsername("root");
+            p.setPassword("gaetano99");
+            p.setMaxActive(100);
+            p.setInitialSize(10);
+            p.setMinIdle(10);
+            p.setRemoveAbandonedTimeout(60);
+            p.setRemoveAbandoned(true);
+            datasource = new DataSource();
+            datasource.setPoolProperties(p);
+        }
+        return datasource.getConnection();
+    }
 }
