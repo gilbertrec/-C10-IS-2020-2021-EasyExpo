ce9cd2d3d3a9f5f6ce1100110266f684fcb06c19
gaetanik99 <iulianogaetano99@gmail.com>
2020-12-28 21:53:38 +0100

    Revert "Rimozione item areaFornitore"
    This reverts commit c7bf5e1e0daf08ede49a9feb3ea3d28f12afc6aa.

--- src/EasyExpo/src/main/java/Controller/ModificaProdottoServlet.java
+++ src/EasyExpo/src/main/java/Controller/ModificaProdottoServlet.java
@@ -1,8 +1,5 @@
 package Controller;
 
-import Model.DAO.ProdottoDAO;
-import Model.POJO.Prodotto;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,10 +15,7 @@ public class ModificaProdottoServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int id = Integer.parseInt(request.getParameter("prodottoId"));
-        ProdottoDAO prodottodao = new ProdottoDAO() ;
-        Prodotto prodotto = prodottodao.doRetrieveByIdProdotto(id);
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/modificaProdotto.jsp");
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/mmodificaProdotto.jsp");
         requestDispatcher.forward(request, response);
     }
 }
