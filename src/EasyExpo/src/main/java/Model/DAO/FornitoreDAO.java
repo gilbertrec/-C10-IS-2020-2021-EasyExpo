4c809d9b0314d5566291635028606eaa8afa2770
LucreziaRob <73703786+LucreziaRob@users.noreply.github.com>
2021-01-23 23:45:22 +0100

    modifica a FornitoreDAO

--- src/EasyExpo/src/main/java/Model/DAO/FornitoreDAO.java
+++ src/EasyExpo/src/main/java/Model/DAO/FornitoreDAO.java
@@ -53,40 +53,6 @@ public class FornitoreDAO {
   }
 
   /**
-   * Metodo che ritorna le istanze di tipo Fornitore contenute nel DB.
-   *
-   * @param offset indice partenza, Intero
-   * @param limit  indice fine , Intero
-   * @return List &lt;Fornitore&gt; - {@link List} di oggetti di tipo {@link Fornitore}
-   */
-  public List<Fornitore> doRetrieveAll(int offset, int limit) {
-    try (Connection con = DBConnection.getConnection()) {
-      PreparedStatement ps = con
-          .prepareStatement("SELECT * FROM Fornitore LIMIT ?, ?");
-      ps.setInt(1, offset);
-      ps.setInt(2, limit);
-      ArrayList<Fornitore> fornitori = new ArrayList<>();
-      ResultSet rs = ps.executeQuery();
-      while (rs.next()) {
-        Fornitore f = new Fornitore();
-        f.setPartitaIva(rs.getString(1));
-        f.setNome(rs.getString(2));
-        f.setCognome(rs.getString(3));
-        f.setEmail(rs.getString(4));
-        f.setPassword(rs.getString(5));
-        f.setTelefono(rs.getString(6));
-        f.setLuogoUbicazione(rs.getString(7));
-        f.setRagioneSociale(rs.getString(8));
-        f.setAbbonato(rs.getBoolean(9));
-        fornitori.add(f);
-      }
-      return fornitori;
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  /**
    * Metodo che crea un'istanza, all'interno del DB, di tipo Fornitore.
    *
    * @param fornitore Oggetto di tipo {@link Fornitore}
@@ -111,37 +77,6 @@ public class FornitoreDAO {
       if (ps.executeUpdate() != 1) {
         throw new RuntimeException("INSERT error.");
       }
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  /**
-   * Metodo che ritorna l'oggetto di tipo Fornitore correlato all'email data in input.
-   *
-   * @param email Indirizzo email identificativo, String
-   * @return Fornitore - Oggetto di tipo {@link Fornitore}
-   */
-  public Fornitore doRetrieveByEmail(String email) {
-    try (Connection con = DBConnection.getConnection()) {
-      PreparedStatement ps = con.prepareStatement(
-          "SELECT * FROM Fornitore WHERE email=?");
-      ps.setString(1, email);
-      ResultSet rs = ps.executeQuery();
-      if (rs.next()) {
-        Fornitore f = new Fornitore();
-        f.setPartitaIva(rs.getString(1));
-        f.setNome(rs.getString(2));
-        f.setCognome(rs.getString(3));
-        f.setEmail(rs.getString(4));
-        f.setPassword(rs.getString(5));
-        f.setTelefono(rs.getString(6));
-        f.setLuogoUbicazione(rs.getString(7));
-        f.setRagioneSociale(rs.getString(8));
-        f.setAbbonato(rs.getBoolean(9));
-        return f;
-      }
-      return null;
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
@@ -178,35 +113,6 @@ public class FornitoreDAO {
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
-  }
-
-  /**
-   * Metodo che ritorna le istanze di tipo Fornitore contenute nel DB.
-   *
-   * @param ricercato , String
-   * @return List &lt;Fornitore&gt; - {@link List} di oggetti di tipo {@link Fornitore}
-   */
-  public List<Fornitore> doRetrieveByNome(String ricercato) {
-    try (Connection con = DBConnection.getConnection()) {
-
-      PreparedStatement ps = con.prepareStatement(
-          "SELECT partitaIva, nome FROM Fornitore WHERE nome LIKE ? ");
-      ps.setString(1, "%" + ricercato + "%");
-
-      ArrayList<Fornitore> fornitore = new ArrayList<>();
-      ResultSet rs = ps.executeQuery();
-      while (rs.next()) {
-        Fornitore f = new Fornitore();
-        f.setPartitaIva(rs.getString(1));
-        f.setNome(rs.getString(2));
-        fornitore.add(f);
-      }
-      return fornitore;
-
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
-    }
-
   }
 
   /**
