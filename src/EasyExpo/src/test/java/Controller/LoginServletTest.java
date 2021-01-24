dd8dfa8efd4c2f79576630e393a338e848edb818
LucreziaRob <73703786+LucreziaRob@users.noreply.github.com>
2021-01-24 19:19:56 +0100

    eliminanzione prove Servlet

--- src/EasyExpo/src/test/java/Controller/LoginServletTest.java
+++ src/EasyExpo/src/test/java/Controller/LoginServletTest.java
@@ -1,54 +1,26 @@
 package Controller;
 
-import Model.POJO.Cliente;
-import org.apache.struts.mock.MockHttpServletRequest;
-import org.apache.struts.mock.MockHttpServletResponse;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
-import javax.servlet.ServletException;
+class LoginServletTest {
 
-import java.io.IOException;
+  @BeforeEach
+  void setUp() {
+  }
 
-import static org.junit.jupiter.api.Assertions.*;
+  @AfterEach
+  void tearDown() {
+  }
 
-class LoginServletTest extends Mockito {
+  @Test
+  void doPost() {
+  }
 
-    private LoginServlet servlet;
-    private MockHttpServletRequest request;
-    private MockHttpServletResponse response;
-
-
-    @BeforeEach //prima di ogni test
-    public void setUp() {
-        servlet = new LoginServlet();
-        request = new MockHttpServletRequest();
-        response = new MockHttpServletResponse();
-        request.addParameter("email", "kattmon00@gmail.com");
-        request.addParameter("password", "KatiaM00_");
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
-    @Test
-    void doPost() {
-
-    }
-
-    @Test
-    void testClienteEmailFail1() throws ServletException, IOException {
-        request.addParameter("email", "k@g.com");
-        String message = "Valore email sbagliata";
-       // Cliente cliente = new Cliente("kattmon00@gmail.com", "KatiaM00_");
-
-
-    }
-
-    @Test
-    void doGet() {
-    }
+  @Test
+  void doGet() {
+  }
 }
