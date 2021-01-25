7223153def3aa7edeaad983ba7a3a6b348c717b3
LucreziaRob <73703786+LucreziaRob@users.noreply.github.com>
2021-01-25 15:50:09 +0100

    PreventiviServletTest

--- src/EasyExpo/src/test/java/Controller/PreventiviServletTest.java
+++ src/EasyExpo/src/test/java/Controller/PreventiviServletTest.java
@@ -1,5 +1,7 @@
 package Controller;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,35 +16,77 @@ import javax.servlet.http.HttpSession;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PreventiviServletTest {
-    private PreventiviServlet preventiviServlet;
-    private HttpServletRequest mockedRequest;
-    private HttpServletResponse mockedResponse;
-    private ServletContext mockedServletContext;
-    private RequestDispatcher mockedDispatcher;
-    private HttpSession mockedSession;
+  private PreventiviServlet preventiviServlet;
+  private HttpServletRequest mockedRequest;
+  private HttpServletResponse mockedResponse;
+  private ServletContext mockedServletContext;
+  private RequestDispatcher mockedDispatcher;
+  private HttpSession mockedSession;
 
-    MyServletException exception = null;
+  MyServletException exception = null;
 
-    @BeforeEach
-    void setUp() {
-        preventiviServlet = new PreventiviServlet();
-        mockedRequest = Mockito.mock(HttpServletRequest.class);
-        mockedResponse = Mockito.mock(HttpServletResponse.class);
-        mockedServletContext = Mockito.mock(ServletContext.class);
-        mockedDispatcher = Mockito.mock(RequestDispatcher.class);
-        mockedSession = Mockito.mock(HttpSession.class);
+  @BeforeEach
+  void setUp() {
+    preventiviServlet = new PreventiviServlet();
+    mockedRequest = Mockito.mock(HttpServletRequest.class);
+    mockedResponse = Mockito.mock(HttpServletResponse.class);
+    mockedServletContext = Mockito.mock(ServletContext.class);
+    mockedDispatcher = Mockito.mock(RequestDispatcher.class);
+    mockedSession = Mockito.mock(HttpSession.class);
 
-    }
+  }
 
-    @AfterEach
-    void tearDown() {
-    }
+  @AfterEach
+  void tearDown() {
+  }
 
-    @Test
-    void doPost() {
-    }
+  @Test
+  void TestClienteNull() throws ServletException, IOException {
+    Mockito.when(mockedRequest.getParameter("codiceFiscale")).thenReturn("IULGAE99C23R123R");
 
-    @Test
-    void doGet() {
-    }
+    Mockito.when(mockedRequest.getSession()).thenReturn(mockedSession);
+    Mockito.doReturn(mockedSession).when(mockedRequest).getSession(true);
+
+    Mockito.doReturn(mockedServletContext).when(mockedRequest).getServletContext();
+    Mockito.doReturn(mockedDispatcher).when(mockedServletContext)
+        .getRequestDispatcher("/listaPreventivi.jsp");
+
+    preventiviServlet.doGet(mockedRequest, mockedResponse);
+
+  }
+
+  @Test
+  void TestClienteFornitoreNull() throws ServletException, IOException {
+
+    Mockito.doReturn(mockedServletContext).when(mockedRequest).getServletContext();
+    Mockito.doReturn(mockedDispatcher).when(mockedServletContext)
+        .getRequestDispatcher("/listaPreventivi.jsp");
+
+    preventiviServlet.doGet(mockedRequest, mockedResponse);
+
+  }
+
+  @Test
+  void TestFornitoreNull() throws ServletException, IOException {
+    Mockito.when(mockedRequest.getParameter("partitaIva")).thenReturn("01391350129");
+
+    Mockito.when(mockedRequest.getSession()).thenReturn(mockedSession);
+    Mockito.doReturn(mockedSession).when(mockedRequest).getSession(true);
+
+    Mockito.doReturn(mockedServletContext).when(mockedRequest).getServletContext();
+    Mockito.doReturn(mockedDispatcher).when(mockedServletContext)
+        .getRequestDispatcher("/listaPreventivi.jsp");
+
+    preventiviServlet.doGet(mockedRequest, mockedResponse);
+
+  }
+
+
+  @Test
+  void doPost() {
+  }
+
+  @Test
+  void doGet() {
+  }
 }
